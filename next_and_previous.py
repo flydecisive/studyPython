fe0d35c9c0f684059c3b91cecaee5032172a2146
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-02-14 21:04:56 +0400

    Update next_and_previous.py
    Исправления для прохождения проверки поо правилам сервиса

--- next_and_previous.py
+++ next_and_previous.py
@@ -1,5 +1,6 @@
 number = int(input())
 next = number + 1
 previous = number - 1
-print("The next number for the number " + str(number) + " is " + str(next) + ".")
-print("The previous number for the number " + str(number) + " is " + str(previous) + ".")
+print("The next number for the number", number, "is", next, end=".")
+print()
+print("The previous number for the number", number, "is", previous, end=".")
