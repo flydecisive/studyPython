448924a5e5b3ed6d54fe635a5dc3d75a845baab7
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-02-14 20:53:26 +0400

    Create next_and_previous.py
    Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (важно в точности соблюдать вывод программы: обратите внимание на пробелы и на точки). Нельзя пользоваться конкатенацией строк, используйте print с несколькими параметрами.

---
+++ next_and_previous.py
@@ -0,0 +1,5 @@
+number = int(input())
+next = number + 1
+previous = number - 1
+print("The next number for the number " + str(number) + " is " + str(next) + ".")
+print("The previous number for the number " + str(number) + " is " + str(previous) + ".")
