3de0029415479ebd624224b955ace1987bfd017b
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-03-04 00:31:11 +0400

    Create world.py
    exercise from book

---
+++ world.py
@@ -0,0 +1,18 @@
+#step 3 - 8
+countries = ["Greece", "Turkey", "Russia", "Scotland", "Germany"]
+print(countries)
+print(sorted(countries))
+print(countries)
+print(sorted(countries, reverse=True))
+print(countries)
+countries.reverse()
+print(countries)
+countries.reverse()
+print(countries)
+countries.sort()
+print(countries)
+countries.sort(reverse=True)
+print(countries)
+
+#step 3- 9
+print("Количество стран - " + str(len(countries)))
