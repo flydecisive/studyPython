19290edd9cbb8ede63706c5cd917e75ff85fc61a
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-03-08 23:41:08 +0400

    Create add_fpr_pizza.py
    exercise from book

---
+++ add_fpr_pizza.py
@@ -0,0 +1,7 @@
+#step 7-4
+offer = 'Введите дополнение для пиццы: '
+add = input(offer)
+while add != 'quit':
+    if add != 'quit':
+        print('Дополнение ' + add + ' включено в заказ.')
+    add = input(offer)
