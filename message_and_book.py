91f6fe66a374fe279918fe3355c1ba113e47f5a3
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-03-10 09:11:32 +0400

    Create message_and_book.py
    exercise from book

---
+++ message_and_book.py
@@ -0,0 +1,12 @@
+#step 8-1
+def display_message():
+    print('Функции')
+
+display_message()
+
+#step 8-2
+def favorite_book(title):
+    print('One of my favorite books is ' + title.title())
+
+title = 'преступление и наказание'
+favorite_book(title)
