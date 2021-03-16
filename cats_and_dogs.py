dde80f912948ca388846f1100800498a2df750c0
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-03-16 23:59:19 +0400

    Update cats_and_dogs.py
    add pass

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -6,3 +6,14 @@ for file in filename:
             print(file_object.read())
     except FileNotFoundError:
         print('Файл ' + file + ' не найден')
+
+
+#step 10-9
+print('\nNext step\n')
+filename = ['cats.txt', 'dogs.txt']
+for file in filename:
+    try:
+        with open(file) as file_object:
+            print(file_object.read())
+    except FileNotFoundError:
+        pass
