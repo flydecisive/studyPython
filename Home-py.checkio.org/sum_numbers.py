01e4d269c1637c4314c92e5ea86ed92b219fc3d9
flydecisive <45427901+flydecisive@users.noreply.github.com>
2021-03-21 12:47:31 +0400

    Update sum_numbers.py
    elementary
    checkio

--- Home-py.checkio.org/sum_numbers.py
+++ Home-py.checkio.org/sum_numbers.py
@@ -2,6 +2,25 @@ def sum_numbers(text):
     '''Функция возвращает сумму чисел в строке, 
         если число не является частью слова'''
     words = text.split(' ')
-    print(words)
+    numbers = []
+    sum = 0
+    for word in words:
+        if word == '':
+            break
+        elif not word.isalpha():
+            count = 0
+            for letter in word:
+                if letter.isalpha():
+                    count += 1
+            if count > 0:
+                continue
+            else:
+                numbers.append(int(word))
 
-sum_numbers('who is first here')
+    for number in numbers:
+        sum += number
+
+    return sum
+    
+
+print(sum_numbers(''))
